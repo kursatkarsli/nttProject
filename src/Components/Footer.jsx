67386b2062bfce29bb0f697b2396e9c99fabb726
Kürşat Karslı <61808010+kursatkarsli@users.noreply.github.com>
2023-02-25 02:17:09 +0300

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,24 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { CardMedia, Grid } from "@mui/material";
-import FooterLogo from '../assets/images/FooterLogo.png';
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  ButtonGroup,
+  CardMedia,
+  Divider,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+} from "@mui/material";
+import FooterLogo from "../assets/images/FooterLogo.png";
+
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const bull = (
   <Box
@@ -19,45 +35,147 @@ const bull = (
 
 export default function Footer() {
   return (
-    <Card sx={{ maxHeight: 400, backgroundColor: "#0059BC", color: "white" }}>
-      <Grid container display={'flex'} justifyContent='space-around' alignItems={'center'}>
-        <Grid item>
+    <Card
+      sx={{
+        maxHeight: 380,
+        backgroundColor: "#0059BC",
+        color: "white",
+      }}
+    >
+      <Grid
+        container
+        display={"flex"}
+        justifyContent="space-around"
+        alignItems={"center"}
+        sx={{ padding: 5 }}
+      >
+        <Grid item display={"flex"} direction="column">
           <CardMedia
-            height="194"
-            width={170}
             src={FooterLogo}
             component="img"
-            sx={{objectFit:'contain'}}
+            sx={{ objectFit: "contain", width: "170px" }}
             alt="Paella dish"
           ></CardMedia>
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} gutterBottom>
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="div">
-              be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography sx={{ mb: 1.5 }}>adjective</Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
+          <CardContent
+            sx={{ paddingInline: 0, maxWidth: 400, marginBottom: "20px" }}
+          >
+            <Typography
+              sx={{
+                fontSize: 14,
+                textOverflow: "ellipsis",
+                whiteSpace: "pre-line",
+              }}
+              gutterBottom
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+              suscipit voluptatem illo eligendi alias, voluptatum quia harum
             </Typography>
           </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
+          <CardActions
+            sx={{
+              display: "flex",
+              padding: 0,
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              placeholder="Email"
+              InputProps={{
+                sx: {
+                  paddingTop: "5px",
+                  "&>input": { paddingTop: "5px" },
+                },
+              }}
+              variant="filled"
+              size="small"
+              sx={{
+                color: "black",
+                backgroundColor: "white",
+                borderColor: "white",
+              }}
+            />
+
+            <Button
+              size="small"
+              sx={{
+                color: "white",
+                background: "#00254F",
+                padding: "8px",
+                borderRadius: "0",
+              }}
+            >
+              Sign Up
+            </Button>
           </CardActions>
         </Grid>
         <Grid item>
-       asdasasds
+          <Typography variant="h6">Title</Typography>
+          <List sx={{ width: "100%", maxWidth: 360 }}>
+            {[1, 2, 3, 4].map((value) => (
+              <ListItem
+                key={value}
+                disableGutters
+                sx={{ paddingTop: 0, paddingBottom: "3px" }}
+              >
+                <ListItemText primary={`Line item ${value}`} />
+              </ListItem>
+            ))}
+          </List>
         </Grid>
         <Grid item>
-        asdadadas
-        </Grid>
+          <Typography variant="h6">Title</Typography>
+          <List sx={{ width: "100%", maxWidth: 360 }}>
+            {[1, 2, 3, 4].map((value) => (
+              <ListItem
+                key={value}
+                disableGutters
+                sx={{ paddingTop: 0, paddingBottom: "3px" }}
+              >
+                <ListItemText primary={`Line item ${value}`} />
+              </ListItem>
+            ))}
+          </List>
+        </Grid>{" "}
         <Grid item>
-      asdasda
+          <Typography variant="h6">Title</Typography>
+          <List sx={{ width: "100%", maxWidth: 360 }}>
+            {[1, 2, 3, 4].map((value) => (
+              <ListItem
+                key={value}
+                disableGutters
+                sx={{ paddingTop: 0, paddingBottom: "3px" }}
+              >
+                <ListItemText primary={`Line item ${value}`} />
+              </ListItem>
+            ))}
+          </List>
         </Grid>
       </Grid>
+      <Divider sx={{backgroundColor:'white'}} />
+
+      <BottomNavigation
+        sx={{
+          justifyContent: "space-around",
+          backgroundColor: "#0059BC",
+          color: "white",
+          alignItems: "center",
+        }}
+      >
+        <Typography>contact@nttdata.com</Typography>
+        <Typography>+3 9876 765 444</Typography>
+
+        <ButtonGroup size="small" aria-label="small button group">
+          <IconButton>
+            <FacebookIcon sx={{ color: 'white' }} />
+          </IconButton>
+          <IconButton>
+            <InstagramIcon sx={{ color: 'white' }} />
+          </IconButton>
+          <IconButton>
+            <LinkedInIcon sx={{ color: 'white' }} />
+          </IconButton>
+        </ButtonGroup>
+      </BottomNavigation>
     </Card>
   );
 }
