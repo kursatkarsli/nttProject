c9c8154c27ce2f260b7fff51d5b6ff01f9c476f0
Kürşat Karslı <61808010+kursatkarsli@users.noreply.github.com>
2023-03-12 21:30:39 +0300

    store fixed

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/products/productsReducer';
+import productsReducer from '../features/products/productsReducer';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    productsReducer: productsReducer,
   },
 });
